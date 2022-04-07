6a3ac6a5c8394496ffc1c0bb5191dd2f6eb47d85
xeangit <mily072@naver.com>
2022-04-07 20:21:17 +0900

    feat: Add HelloController

---
+++ src/main/java/com/web/community/springboot/web/HelloController.java
@@ -0,0 +1,12 @@
+package com.web.community.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
