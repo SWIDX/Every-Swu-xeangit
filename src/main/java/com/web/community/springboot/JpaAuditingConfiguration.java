2ba260974f33eab5a4afe5b2ff3d65253959895d
xeangit <mily072@naver.com>
2022-05-18 14:47:53 +0900

    fix: Add JpaAuditing file

---
+++ src/main/java/com/web/community/springboot/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package com.web.community.springboot;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+}
