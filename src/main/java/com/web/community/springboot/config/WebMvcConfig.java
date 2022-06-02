5b443efa35d04065120c92fd09f58c2541f03243
xeangit <mily072@naver.com>
2022-06-02 16:09:20 +0900

    chore: Add delete posts

--- src/main/java/com/web/community/springboot/config/WebMvcConfig.java
+++ src/main/java/com/web/community/springboot/config/WebMvcConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     private final long MAX_AGE_SECS = 3600;
 
-//    @Override
+
 //    public void addCorsMapping(CorsRegistry registry){
 //        registry.addMapping("/**")
 //                .allowedOrigins("http://localhost:3000")
